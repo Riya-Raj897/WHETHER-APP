56e1152540474455d39bdb2ff4592dd832a11240
Riya-Raj897 <riya15367@gmail.com>
2024-04-20 03:29:06 -0700

    bugfixed

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const weather_img=document.querySelector('.weather-img');
 const temperature=document.querySelector('.temperature');
 const description=document.querySelector('.description');
 const humidity=document.getElementById('humidity');
-const wind_speed=ducument.getElementById('wind-speed');
+const wind_speed=document.getElementById('wind-speed');
 
 const location_not_found = document.querySelector('.location-not-found');
 const weather_body = document.querySelector('.weather-body');
@@ -30,7 +30,7 @@ weather_body.style.display="flex";
 
         temperature.innerHTML=`${Math.round(weather_data.main.temp-273.15)}°C`;
         description.innerHTML=`${weather_data.weather[0].description}`;
-        humidity.innerHTML= `${weather_data.main,humidity}%`;
+        humidity.innerHTML= `${weather_data.main.humidity}%`;
         wind_speed.innerHTML=`${weather_data.wind.speed}Km/H`;
 
 
